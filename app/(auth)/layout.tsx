8c1623dc51e58dae25a11b81ea5bc313587a2c17
abunaim34 <farhanadnanfarabi@gmail.com>
2026-07-18 19:00:22 +0600

    Creating Login Form With ShadCn

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+
+const LoginLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="max-w-7xl mx-auto">
+            {children}
+        </div>
+    )
+}
+
+export default LoginLayout
