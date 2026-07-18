ca540f844a3871b70db3756b2c3bed910e2cc631
abunaim34 <farhanadnanfarabi@gmail.com>
2026-07-18 19:05:58 +0600

    regiter page design

---
+++ app/(auth)/_components/RegisterForm.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
+const RegisterForm = () => {
+    return (
+        <div className="space-y-6">
+            <Card className="p-4 space-y-4">
+                <Input name="email" type="email" placeholder="Enter Your Email" required />
+                <Input name="password" type="password" placeholder="Enter Your Password" required />
+                <Button type="submit">Login</Button>
+            </Card>
+        </div>
+    );
+};
+
+export default RegisterForm;
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,7 +1,19 @@
+import RegisterForm from "../_components/RegisterForm"
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="flex min-h-screen items-center justify-center">
+            <div className="w-full max-w-md space-y-6 rounded-lg border p-8 shadow-lg">
+                {/* Form Generic Texts */}
+                <div className="space-y-2 text-center">
+                    <h1 className="text-3xl font-bold">Welcome to Our Next Press</h1>
+                    <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod excepturi quasi et? Doloremque autem eos praesentium repellat iure natus sequi.</p>
+                </div>
+
+                {/* Form */}
+                <RegisterForm />
+            </div>
+        </div>
   )
 }
 
