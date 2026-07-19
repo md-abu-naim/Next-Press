0f0492148d2a2bb370623262003198f761663210
abunaim34 <farhanadnanfarabi@gmail.com>
2026-07-19 18:31:35 +0600

    Implementing Server Action For Login Form

--- app/(auth)/_components/LoginForm.tsx
+++ app/(auth)/_components/LoginForm.tsx
@@ -1,16 +1,18 @@
+"use client"
 import { Button } from "@/components/ui/button"
 import { Card } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
+import { LoginAction } from "../_actions/authActions"
 
 const LoginForm = () => {
     return (
-        <div className="space-y-6">
+        <form action={LoginAction} className="space-y-6">
             <Card className="p-4 space-y-4">
                 <Input name="email" type="email" placeholder="Enter Your Email" required />
                 <Input name="password" type="password" placeholder="Enter Your Password" required />
                 <Button type="submit">Login</Button>
             </Card>
-        </div>
+        </form>
     )
 }
 
