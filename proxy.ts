b29fd957ea4e4f3bb5775b2d1f93f5155a30b857
abunaim34 <farhanadnanfarabi@gmail.com>
2026-07-21 17:58:18 +0600

    Handling Auth Routes In Proxy File

--- proxy.ts
+++ proxy.ts
@@ -1,16 +1,38 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import jwt, { JwtPayload } from 'jsonwebtoken'
 
-// This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
+const AUTH_ROUTES = ['/login', '/register']
+
+export async function proxy(request: NextRequest) {
     const pathname = request.nextUrl.pathname
-    console.log(pathname);
-    // return NextResponse.redirect(new URL('/', request.url))
+
+    const accessToken = request.cookies.get("accessToken")?.value
+
+    const decodedToken = accessToken ? jwt.decode(accessToken) as JwtPayload : null
+
+    let userRole = null
+
+    if (decodedToken) {
+        userRole = decodedToken.role
+    }
+
+    if (accessToken && AUTH_ROUTES.includes(pathname)) {
+        if (userRole === 'USER') {
+            return NextResponse.redirect(new URL('/dashboard', request.url))
+        }
+        else if (userRole === 'AUTHOR') {
+            return NextResponse.redirect(new URL('/author-dashboard', request.url))
+        }
+        else if (userRole === 'ADMIN') {
+            return NextResponse.redirect(new URL('/admin-dashboard', request.url))
+        } else {
+            return NextResponse.redirect(new URL('/', request.url))
+        }
+    }
+
     return NextResponse.next()
 }
-
-// Alternatively, you can use a default export:
-// export default function proxy(request: NextRequest) { ... }
 
 export const config = {
     matcher: [
