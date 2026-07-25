5a31695f5519608b35cda159cfeaa60257b2cb4c
abunaim34 <farhanadnanfarabi@gmail.com>
2026-07-25 20:08:19 +0800

    Implementing URL Based State Handling For Searching News

--- app/(public)/_actions/getSubscriptionStatus.ts
+++ app/(public)/_actions/getSubscriptionStatus.ts
@@ -3,18 +3,18 @@
 import { cookies } from "next/headers";
 
 export const getSubscriptionStatus = async () => {
-     const cookieStore = await cookies();
-    
-        const accessToken = cookieStore.get("accessToken")?.value || null;
-    
-        if(!accessToken){
-            // throw new Error("User Not Logged In!");
-    
-            return {
-                success : false,
-                message : "User not logged in!"
-            }
+    const cookieStore = await cookies();
+
+    const accessToken = cookieStore.get("accessToken")?.value || null;
+
+    if (!accessToken) {
+        // throw new Error("User Not Logged In!");
+
+        return {
+            success: false,
+            message: "User not logged in!"
         }
+    }
 
     const res = await fetch(`${process.env.BACKEND_URL}/api/subscription/stats`, {
         headers: {
