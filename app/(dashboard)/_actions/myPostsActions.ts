9c24c9453e6f6a9d07a45f2c44faaf188522883d
abunaim34 <farhanadnanfarabi@gmail.com>
2026-07-26 13:23:25 +0800

    Implementing Functionalities Of Edit News / Post Form Modal / Dialog With Cache Invalidation

--- app/(dashboard)/_actions/myPostsActions.ts
+++ app/(dashboard)/_actions/myPostsActions.ts
@@ -11,7 +11,7 @@ type loginState = {
     data: Record<string, any>
 }
 
-export const createPost = async(PreveState: loginState, formData: FormData) => {
+export const createPost = async (PreveState: loginState, formData: FormData) => {
     const payload = {
         title: formData.get('title'),
         content: formData.get('content'),
@@ -42,14 +42,70 @@ export const createPost = async(PreveState: loginState, formData: FormData) => {
 
     const result = await res.json()
 
-    if(result.success){
-        revalidateTag('my-posts', 'max')
+    if (result.success) {
+        revalidateTag('my-posts', {
+            expire: 0
+        })
     }
 
-    if(result.success && result.data.isPremium){
-        revalidateTag('premium-posts', 'max')
-    }else{
-        revalidateTag('public-posts', 'max')
+    if (result.success && result.data.isPremium) {
+        revalidateTag('premium-posts', {
+            expire: 0
+        })
+    } else {
+        revalidateTag('public-posts', {
+            expire: 0
+        })
+    }
+
+    return result
+}
+
+export const updatePost = async (postId: string, PreveState: loginState, formData: FormData) => {
+    const payload = {
+        title: formData.get('title') ?? "",
+        content: formData.get('content') ?? "",
+        thumbnail: formData.get('thumbnail') ?? "",
+        tags: (formData.get("tags") as string).split(', ') ?? "",
+        isPremium: formData.get("isPremium") === 'on'
+    };
+
+    const cookieStore = await cookies()
+
+    const accessToken = cookieStore.get('accessToken')?.value
+
+    if (!accessToken) {
+        return {
+            success: false,
+            message: 'User not Logged in!'
+        }
+    }
+
+    const res = await fetch(`${process.env.BACKEND_URL}/api/posts/${postId}`, {
+        method: "PATCH",
+        headers: {
+            Cookie: `accessToken=${accessToken}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(payload)
+    })
+
+    const result = await res.json()
+
+    if (result.success) {
+        revalidateTag('my-posts', {
+            expire: 0
+        })
+    }
+
+    if (result.success && result.data.isPremium) {
+        revalidateTag('premium-posts', {
+            expire: 0
+        })
+    } else {
+        revalidateTag('public-posts', {
+            expire: 0
+        })
     }
 
     return result
