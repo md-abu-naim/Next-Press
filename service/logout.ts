ff18c07d75092db93e38da3b5f4d3b2c677cfda9
abunaim34 <farhanadnanfarabi@gmail.com>
2026-07-27 14:13:09 +0800

    project completed

--- service/logout.ts
+++ service/logout.ts
@@ -4,7 +4,7 @@ import { revalidateTag } from "next/cache"
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 
-export const logout = async() => {
+export const logout = async () => {
     const cookieStore = await cookies()
 
     cookieStore.delete('accessToken')
